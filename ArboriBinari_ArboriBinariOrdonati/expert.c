ee70664c1064e0a8b3aabb23224dc2d00fb766e2
FlaviusMarian27 <marianflavius27@yahoo.com>
2025-06-22 18:35:53 +0300

    Adaugare problema examen sistem expert

---
+++ ArboriBinari_ArboriBinariOrdonati/expert.c
@@ -0,0 +1,22 @@
+/*
+1. Implementare C99 + explicații pe foaie
+Ai un fișier cu un arbore de întrebări (sistemul expert) — fiecare nod are două 
+ieșiri: una pentru „NU” și una pentru „DA”.
+
+Ai un alt fișier cu răspunsurile pacientului (șir de 0 și 1).
+Trebuie să simulezi consultația și să determini:
+dacă se ajunge la un diagnostic (frunză, adică nod fără alte ramuri)
+altfel, câte întrebări maxim ar mai fi necesare ca să ajungi garantat la un diagnostic.
+
+2. Determinare traseu maxim (independent de răspunsuri)
+Se cere să afișezi numărul maxim de întrebări care pot fi parcurse din rădăcină până la 
+un diagnostic (adică adâncimea maximă a arborelui de decizie).
+
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    return 0;
+}
